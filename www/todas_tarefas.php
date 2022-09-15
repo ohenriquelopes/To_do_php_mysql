b1498f964d9c6822e4d736d714784071a53ebb1c
Henrique l <hlopessantos52@gmail.com>
2022-09-15 19:36:36 -0300

    add project curso

--- www/todas_tarefas.php
+++ www/todas_tarefas.php
@@ -21,23 +21,23 @@
 
 		<div class="container app">
 			<div class="row">
-				<div class="col-md-3 menu">
+				<div class="col-sm-3 menu">
 					<ul class="list-group">
-						<li class="list-group-item active"><a href="#">Tarefas pendentes</a></li>
+						<li class="list-group-item"><a href="index.php">Tarefas pendentes</a></li>
 						<li class="list-group-item"><a href="nova_tarefa.php">Nova tarefa</a></li>
-						<li class="list-group-item"><a href="todas_tarefas.php">Todas tarefas</a></li>
+						<li class="list-group-item active"><a href="#">Todas tarefas</a></li>
 					</ul>
 				</div>
 
-				<div class="col-md-9">
+				<div class="col-sm-9">
 					<div class="container pagina">
 						<div class="row">
 							<div class="col">
-								<h4>Tarefas pendentes</h4>
+								<h4>Todas tarefas</h4>
 								<hr />
 
 								<div class="row mb-3 d-flex align-items-center tarefa">
-									<div class="col-sm-9">Lavar o carro</div>
+									<div class="col-sm-9">Lavar o carro (status)</div>
 									<div class="col-sm-3 mt-2 d-flex justify-content-between">
 										<i class="fas fa-trash-alt fa-lg text-danger"></i>
 										<i class="fas fa-edit fa-lg text-info"></i>
@@ -46,13 +46,14 @@
 								</div>
 
 								<div class="row mb-3 d-flex align-items-center tarefa">
-									<div class="col-sm-9">Passear com o cachorro</div>
+									<div class="col-sm-9">Passear com o cachorro (status)</div>
 									<div class="col-sm-3 mt-2 d-flex justify-content-between">
 										<i class="fas fa-trash-alt fa-lg text-danger"></i>
 										<i class="fas fa-edit fa-lg text-info"></i>
 										<i class="fas fa-check-square fa-lg text-success"></i>
 									</div>
 								</div>
+								
 							</div>
 						</div>
 					</div>
