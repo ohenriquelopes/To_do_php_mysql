6f84b38a25e0f08f76f9123ffad7416d31852ea6
Henrique l <hlopessantos52@gmail.com>
2022-09-20 09:42:25 -0300

    modf

--- www/tarefa_controller.php
+++ www/tarefa_controller.php
@@ -5,16 +5,19 @@
     require "conexao.php";
     require "tarefa.service.php";
 
-
-
-    $tarefa = new Tarefa();
-    $tarefa->__set('tarefa', $_POST['tarefa']);
-
-    $conexao = new Conexao();
-
-    $tarefaService = new TarefaService($conexao, $tarefa);
-    $tarefaService->inserir();
+    $acao = isset($_GET['acao']) ? $_GET['acao'] : $acao;
     
-    
+    if($_GET['acao'] == 'inserir' ) {
+        $tarefa = new Tarefa();
+        $tarefa->__set('tarefa', $_POST['tarefa']);
 
+        $conexao = new Conexao();
+
+        $tarefaService = new TarefaService($conexao, $tarefa);
+        $tarefaService->inserir();
+        
+        header('Location: nova_tarefa.php?inclusao=1');
+    } else if ($acao == 'recuperar') {
+        echo "Chegamos ate aqui";
+    }
 ?>  
